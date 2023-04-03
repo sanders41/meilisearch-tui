5b5ebaa17efafce381ff7fffef070c2d795226fd
Paul Sanders <psanders1@gmail.com>
2023-04-03 00:05:49 -0400

    Setup getting index and loading from sidebar

--- meilisearch_tui/screens/meilisearch_settings.py
+++ meilisearch_tui/screens/meilisearch_settings.py
@@ -1,18 +1,20 @@
 from __future__ import annotations
 
-from meilisearch_python_async.errors import MeilisearchCommunicationError
 from textual import events
 from textual.app import ComposeResult
 from textual.containers import Container
 from textual.screen import Screen
 from textual.widgets import Footer, Static
 
-from meilisearch_tui.client import get_client
 from meilisearch_tui.widgets.index_sidebar import IndexSidebar
 from meilisearch_tui.widgets.messages import ErrorMessage
 
 
 class MeilisearchSettings(Screen):
+    def __init__(self, *, id: str | None = None, classes: str | None = None) -> None:
+        super().__init__(id=id, classes=classes)
+        self.selected_index: str | None = None
+
     def compose(self) -> ComposeResult:
         yield ErrorMessage("", classes="message-centered", id="generic-error")
         yield IndexSidebar(classes="sidebar")
@@ -20,31 +22,19 @@ class MeilisearchSettings(Screen):
             yield Static("start", id="test")
         yield Footer()
 
-    # def on_mount(self) -> None:
-    #     print(f"{self.query_one('#index-sidebar').selected_index=}")
-    #     self.query_one("#test").value = self.query_one("#index-sidebar").selected_index
-
     async def on_screen_resume(self, event: events.ScreenResume) -> None:
         body_container = self.query_one("#body", Container)
         error_message = self.query_one("#generic-error", ErrorMessage)
         body_container.visible = True
         error_message.display = False
-        try:
-            async with get_client() as client:
-                indexes = await client.get_indexes()
-        except MeilisearchCommunicationError as e:
-            body_container.visible = False
-            error_message.display = True
-            error_message.renderable = f"An error occured: {e}.\nMake sure the Meilisearch server is running and accessable"  # type: ignore
-            return
-        except Exception as e:
-            body_container.visible = False
-            error_message.display = True
-            error_message.renderable = f"An error occured: {e}."  # type: ignore
-            return
-
         index_sidebar = self.query_one(IndexSidebar)
-        if indexes:
-            await index_sidebar.update([x.uid for x in indexes])
-        else:
-            await index_sidebar.update()
+        await index_sidebar.update()
+        self.selected_index = index_sidebar.selected_index
+        if self.selected_index:
+            self.query_one("#test", Static).update(self.selected_index)
+
+    def on_list_item__child_clicked(self, message: IndexSidebar.Selected) -> None:  # type: ignore[name-defined]
+        index_sidebar = self.query_one(IndexSidebar)
+        self.selected_index = index_sidebar.selected_index
+        if self.selected_index:
+            self.query_one("#test", Static).update(self.selected_index)
--- meilisearch_tui/widgets/index_sidebar.py
+++ meilisearch_tui/widgets/index_sidebar.py
@@ -1,21 +1,47 @@
 from __future__ import annotations
 
+from meilisearch_python_async.errors import MeilisearchCommunicationError
 from textual.app import ComposeResult
 from textual.widgets import Label, ListItem, ListView
 
+from meilisearch_tui.client import get_client
+
 
 class IndexSidebar(ListView):
-    def __init__(self, classes: str) -> None:
-        super().__init__(classes=classes)
+    def __init__(self, *, id: str | None = None, classes: str | None = None) -> None:
+        super().__init__(id=id, classes=classes)
+        self.indexes: list[str] = []
 
     def compose(self) -> ComposeResult:
-        yield ListItem(Label("No indexes"))
+        yield ListItem(Label("No Index retrieval"))
 
-    async def update(self, index_choices: list[str] | None = None) -> None:
+    @property
+    def selected_index(self) -> str | None:
+        if self.index is not None and 0 <= self.index < len(self.indexes):
+            return self.indexes[self.index]
+
+        return None
+
+    async def update(self) -> None:
         await self.clear()
-        if not index_choices:
-            await self.append(ListItem(Label("No indexes")))
-            return None
+        try:
+            async with get_client() as client:
+                indexes = await client.get_indexes()
+        except MeilisearchCommunicationError:
+            await self.append(ListItem(Label("Error connecting to server")))
+            self.indexes = []
+            return
+        except Exception:
+            await self.append(ListItem(Label("Error retrieving indexes")))
+            self.indexes = []
+            return
 
-        for index in index_choices:
-            await self.append(ListItem(Label(index)))
+        if not indexes:
+            await self.append(ListItem(Label("No indexes")))
+            self.indexes = []
+            return
+
+        for index in indexes:
+            self.indexes = [x.uid for x in indexes]
+            await self.append(ListItem(Label(index.uid)))
+        self.index = 0
