9384005530eaf530a79aafa14a365da5916b2890
Paul Sanders <psanders1@gmail.com>
2023-04-03 09:23:50 -0400

    Add viewing of index settings

--- meilisearch_tui/screens/meilisearch_settings.py
+++ meilisearch_tui/screens/meilisearch_settings.py
@@ -1,11 +1,17 @@
 from __future__ import annotations
 
+import asyncio
+
+from meilisearch_python_async.models.settings import (
+    MeilisearchSettings as MeilisearchSettingsResult,
+)
 from textual import events
 from textual.app import ComposeResult
-from textual.containers import Container
+from textual.containers import Container, Content
 from textual.screen import Screen
-from textual.widgets import Footer, Static
+from textual.widgets import Footer, Markdown, Static
 
+from meilisearch_tui.client import get_client
 from meilisearch_tui.widgets.index_sidebar import IndexSidebar
 from meilisearch_tui.widgets.messages import ErrorMessage
 
@@ -20,6 +26,8 @@ class MeilisearchSettings(Screen):
         yield IndexSidebar(classes="sidebar")
         with Container(id="body"):
             yield Static("start", id="test")
+            with Content(id="results-container"):
+                yield Markdown(id="results")
         yield Footer()
 
     async def on_screen_resume(self, event: events.ScreenResume) -> None:
@@ -33,8 +41,44 @@ class MeilisearchSettings(Screen):
         if self.selected_index:
             self.query_one("#test", Static).update(self.selected_index)
 
-    def on_list_item__child_clicked(self, message: IndexSidebar.Selected) -> None:  # type: ignore[name-defined]
+        asyncio.create_task(self.load_indexes())
+
+    async def on_list_item__child_clicked(self, message: IndexSidebar.Selected) -> None:  # type: ignore[name-defined]
         index_sidebar = self.query_one(IndexSidebar)
         self.selected_index = index_sidebar.selected_index
         if self.selected_index:
             self.query_one("#test", Static).update(self.selected_index)
+
+        asyncio.create_task(self.load_indexes())
+
+    async def load_indexes(self) -> None:
+        # save the selected index at the start to make sure it hasn't changed during the request
+        current_index = self.selected_index
+        results_box = self.query_one("#results", Markdown)
+
+        if not self.selected_index:
+            await results_box.update("No index selected")
+            return
+
+        async with get_client() as client:
+            index = client.index(self.selected_index)
+            try:
+                results = await index.get_settings()
+            except Exception as e:
+                if current_index == self.selected_index:
+                    await results_box.update(f"Error: {e}")
+                return
+
+        if current_index == self.selected_index:
+            markdown = self.make_word_markdown(current_index, results)
+            await results_box.update(markdown)
+
+    def make_word_markdown(self, index: str, results: MeilisearchSettingsResult) -> str:
+        lines = []
+
+        lines.append(f"# Settigns for {index} index")
+
+        for k, v in results.dict().items():
+            lines.append(f"## {k}\n{v}\n")
+
+        return "\n".join(lines)
