3c26b6a4ab5ab0556232b83126ae2efc05247ccd
Paul Sanders <psanders1@gmail.com>
2023-04-03 20:56:35 -0400

    Cache element queries for improved performance

--- meilisearch_tui/screens/search.py
+++ meilisearch_tui/screens/search.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import asyncio
+from functools import cached_property
 
 from meilisearch_python_async.errors import MeilisearchCommunicationError
 from meilisearch_python_async.models.search import SearchResults
@@ -33,96 +34,120 @@ class SearchScreen(Screen):
                 yield Button(label="Load More", classes="bottom-spacer", id="load-more-button")
         yield Footer()
 
+    @cached_property
+    def body_container(self) -> Container:
+        return self.query_one("#body", Container)
+
+    @cached_property
+    def generic_error(self) -> ErrorMessage:
+        return self.query_one("#generic-error", ErrorMessage)
+
+    @cached_property
+    def index_sidebar(self) -> IndexSidebar:
+        return self.query_one(IndexSidebar)
+
+    @cached_property
+    def index_name(self) -> Static:
+        return self.query_one("#index-name", Static)
+
+    @cached_property
+    def search_input(self) -> Input:
+        return self.query_one("#search", Input)
+
+    @cached_property
+    def results_container(self) -> Content:
+        return self.query_one("#results-container", Content)
+
+    @cached_property
+    def results(self) -> Markdown:
+        return self.query_one("#results", Markdown)
+
+    @cached_property
+    def load_more_button(self) -> Button:
+        return self.query_one("#load-more-button", Button)
+
     async def on_screen_resume(self, event: events.ScreenResume) -> None:
-        body_container = self.query_one("#body")
-        error_message = self.query_one("#generic-error")
-        body_container.visible = True
-        error_message.display = False
-        index_name = self.query_one("#index-name", Static)
-        index_sidebar = self.query_one(IndexSidebar)
-        await index_sidebar.update()
-        search = self.query_one("#search", Input)
-        search.value = ""
-        await self.query_one("#results", Markdown).update("")
+        self.body_container.visible = True
+        self.generic_error.display = False
+        await self.index_sidebar.update()
+        self.search_input.value = ""
+        await self.results.update("")
         try:
             async with get_client() as client:
                 indexes = await client.get_indexes()
         except MeilisearchCommunicationError as e:
-            body_container.visible = False
-            error_message.display = True
-            error_message.renderable = f"An error occured: {e}.\nMake sure the Meilisearch server is running and accessable"  # type: ignore
+            self.body_container.visible = False
+            self.generic_error.display = True
+            self.generic_error.renderable = f"An error occured: {e}.\nMake sure the Meilisearch server is running and accessable"  # type: ignore
             return
         except Exception as e:
-            body_container.visible = False
-            error_message.display = True
-            error_message.renderable = f"An error occured: {e}."  # type: ignore
+            self.body_container.visible = False
+            self.generic_error.display = True
+            self.generic_error.renderable = f"An error occured: {e}."  # type: ignore
             return
 
         if indexes:
-            index_name.update(f"Searching index: {indexes[0].uid}")
-            self.selected_index = index_sidebar.selected_index
+            self.index_name.update(f"Searching index: {indexes[0].uid}")
+            self.selected_index = self.index_sidebar.selected_index
         else:
             self.selected_index = None
-            self.query_one("#index-name", Static).update("No index selected")
+            self.index_name.update("No index selected")
 
-        search.focus()
-        self.query_one("#load-more-button", Button).visible = False
+        self.search_input.focus()
+        self.load_more_button.visible = False
 
     async def on_list_item__child_clicked(self, message: IndexSidebar.Selected) -> None:  # type: ignore[name-defined]
-        index_sidebar = self.query_one(IndexSidebar)
-        self.selected_index = index_sidebar.selected_index
-        self.query_one("#index-name", Static).update(f"Searching index: {self.selected_index}")
-        self.query_one("#search", Input).value = ""
-        await self.query_one("#results", Markdown).update("")
+        self.selected_index = self.index_sidebar.selected_index
+        self.index_name.update(f"Searching index: {self.selected_index}")
+        self.search_input.value = ""
+        await self.results.update("")
 
     async def on_input_changed(self, message: Input.Changed) -> None:
         self.limit = 20
         if message.value:
             asyncio.create_task(self.lookup_word(message.value))
         else:
-            await self.query_one("#results", Markdown).update("")
-            self.query_one("#load-more-button", Button).visible = False
+            await self.results.update("")
+            self.load_more_button.visible = False
 
     async def on_button_pressed(self, event: Button.Pressed) -> None:
         button_id = event.button.id
 
         if button_id == "load-more-button":
             self.limit += 20
-            asyncio.create_task(self.lookup_word(self.query_one("#search", Input).value))
+            asyncio.create_task(self.lookup_word(self.search_input.value))
 
     async def lookup_word(self, search: str) -> None:
-        results_box = self.query_one("#results", Markdown)
-        search_input = self.query_one("#search", Input)
-        if not self.selected_index and search == search_input.value:
-            await results_box.update("Error: No index provided")
+        if not self.selected_index and search == self.search_input.value:
+            await self.results.update("Error: No index provided")
             return
 
         if not self.selected_index:
-            await results_box.update("No index selected")
+            await self.results.update("No index selected")
             return
 
         async with get_client() as client:
             index = client.index(self.selected_index)
             try:
-                results = await index.search(search_input.value, limit=self.limit)
+                results = await index.search(self.search_input.value, limit=self.limit)
             except Exception as e:
-                if search == search_input.value:
-                    await results_box.update(f"Error: {e}")
+                if search == self.search_input.value:
+                    await self.results.update(f"Error: {e}")
                 return
 
         # Make sure a new search hasn't started. This prevents race conditions with displaying
         # the search results by only updating the display if the search is still relavent.
-        if search == search_input.value:
+        if search == self.search_input.value:
             markdown = self.make_word_markdown(results)
-            await results_box.update(markdown)
+            await self.results.update(markdown)
 
     def make_word_markdown(self, results: SearchResults) -> str:
         lines = []
 
         if results.estimated_total_hits and results.estimated_total_hits > len(results.hits):
-            self.query_one("#load-more-button", Button).visible = True
+            self.load_more_button.visible = True
         else:
-            self.query_one("#load-more-button", Button).visible = False
+            self.load_more_button.visible = False
 
         lines.append(
             f"## Hits: ~{results.estimated_total_hits} | Search time: {results.processing_time_ms} ms"
