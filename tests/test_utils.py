205dcdc394a19523331fe1f925dae4ab944e1d94
Paul Sanders <psanders1@gmail.com>
2023-04-14 05:14:17 -0400

    Add CI tests for Mac and Windows where possible

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -18,6 +18,7 @@ from meilisearch_tui.utils import get_current_indexes_string, string_to_list
     ],
 )
 @pytest.mark.usefixtures("clear_indexes", "mock_config", "env_vars")
+@pytest.mark.meilisearch
 async def test_get_current_indexes_string(indexes, expected, test_client):
     for index in indexes:
         await test_client.create_index(index[0], index[1])
@@ -28,6 +29,7 @@ async def test_get_current_indexes_string(indexes, expected, test_client):
 
 
 @pytest.mark.usefixtures("mock_config", "env_vars")
+@pytest.mark.meilisearch
 async def test_get_current_indexes_string_no_indexes():
     result = await get_current_indexes_string()
 
